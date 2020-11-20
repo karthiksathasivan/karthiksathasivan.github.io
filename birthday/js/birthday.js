d19299d72d54cb959c5155460e243aaa0fa806d8
Karthik <eyank1195@gmail.com>
2020-11-20 17:13:30 +1100

    Update birthday.js

--- birthday/js/birthday.js
+++ birthday/js/birthday.js
@@ -32,7 +32,7 @@ function loadPage(page) {
 
     $.ajax({ url: 'pages/page' + page + '.html' }).
     done(function(pageHtml) {
-        $('.sj-book .p' + page).html(pageHtml.replace('birthday/', ''));
+        $('.sj-book .p' + page).html(pageHtml.replace('samples/steve-jobs/', ''));
     });
 
 }
